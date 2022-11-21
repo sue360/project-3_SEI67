50c4e00eb2f9b65011a7498a258eff0834de1d86
Sussie Anie <sussieanie@hotmail.com>
2022-11-21 10:33:33 +0000

    delete reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -20,3 +20,23 @@ export const addReview = async (req, res) => {
     sendErrors(res, err)
   }
 }
+
+// ? ************ DELETE REVIEW *******************
+// Method: DELETE
+// Endpoint: /projects/:id/reviews/:reviewId - 
+export const deleteReview = async (req, res) => {
+  try {
+    const project = await findProject(req, res)
+    if(project){
+      const { reviewId } = req.params
+      const foundReview = project.reviews.id(reviewId)
+      if (!foundReview) throw new NotFound('Review not found')
+      if (!req.currentUser._id.equals(foundReview.owner)) throw new Unauthorised()
+      await foundReview.remove()
+      await project.save()
+      return res.sendStatus(204)
+    }
+  } catch (err) {
+    sendErrors(res, err)
+  }
+}

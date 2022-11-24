3d624a824ffeacf34b18d37e5c9acb5d8854d6ce
“Mag <margherita.varosio@gmail.com>
2022-11-24 20:51:54 +0000

    Update Profile

--- db/data/projects.js
+++ db/data/projects.js
@@ -1,47 +1,47 @@
 
 export default [
   {
-    projectName: 'Pink Egg with a Saint',
+    name: 'Pink Egg with a Saint',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290819/art_project_api/pic8_tu4xcs.webp',
     year: 2022,
   },
   {
-    projectName: 'Dark Cave',
+    name: 'Dark Cave',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290817/art_project_api/pic4_tze8tm.webp',
     year: 2022,
   },
   {
-    projectName: 'Gorilla on Vacation',
+    name: 'Gorilla on Vacation',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290817/art_project_api/pic3_uraaqn.webp',
     year: 2022,
   },
   {
-    projectName: 'Blue Alien on Sand',
+    name: 'Blue Alien on Sand',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290816/art_project_api/pic2_nc9r9c.webp',
     year: 2022,
   },
   {
-    projectName: 'Joker Bowie',
+    name: 'Joker Bowie',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290814/art_project_api/pic5_oiwedx.webp',
     year: 2022,
   },
   {
-    projectName: 'Alient Ant',
+    name: 'Alient Ant',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290814/art_project_api/pic7_ue9lev.webp',
     year: 2022,
   },
   {
-    projectName: 'Alien Anatomy',
+    name: 'Alien Anatomy',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290814/art_project_api/pic6_wqhxio.webp',
     year: 2022,
   },
   {
-    projectName: 'Monk',
+    name: 'Monk',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290814/art_project_api/pic9_uhhqnb.webp',
     year: 2022,
   },
   {
-    projectName: 'Human Calligraphy',
+    name: 'Human Calligraphy',
     image: 'https://res.cloudinary.com/dcmxao1tc/image/upload/v1669290813/art_project_api/pic1_o32ehx.webp',
     year: 2022,
   }
